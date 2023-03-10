23b61afbfdb015b39da4b313fc22acfc0bd44898
Little Kid <74088371+thaiducloi2000@users.noreply.github.com>
2023-03-10 09:50:53 +0700

    Login with API

--- Assets/Scripts/Connection_Helper/Server_Connection_Helper.cs
+++ Assets/Scripts/Connection_Helper/Server_Connection_Helper.cs
@@ -2,15 +2,22 @@ using System;
 using UnityEngine.Networking;
 using UnityEngine;
 using System.Collections;
+using System.Net;
+using System.Text;
+
 
 public class Server_Connection_Helper : MonoBehaviour 
 {
     private const string BASE_URL = "https://mobilebasedcashflowapi.herokuapp.com/api/";
 
-    public IEnumerator Post(string endpoint, WWWForm form, Action<UnityWebRequest,float> callback)
+    public IEnumerator Post(string endpoint, WWWForm form,string bodydata, Action<UnityWebRequest,float> callback)
     {
+
         using (UnityWebRequest request = UnityWebRequest.Post(BASE_URL + endpoint, form))
         {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(bodydata);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.SetRequestHeader("Content-Type", "application/json");
             yield return request.SendWebRequest();
             callback(request,request.downloadProgress);
         }
